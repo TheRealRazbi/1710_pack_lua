9909aad995b96abe8bd159660c9e899a3bc9158d
Razbi <38946621+TheRealRazbi@users.noreply.github.com>
2026-01-31 20:22:05 +0200

    update

--- main.lua
+++ main.lua
@@ -1,5 +1,5 @@
 function main()
-    print("Version 0.1.0.dev3")
+    print("Version 0.1.0.dev4")
     print(peripheral.isPresent("right"))
     print(peripheral.getMethods("right"))
 end
--- main.py
+++ main.py
@@ -2,6 +2,6 @@ from cc_lib import peripheral
 
 
 def main():
-    print("Version 0.1.0.dev3")
+    print("Version 0.1.0.dev4")
     print(peripheral.is_present("right"))
     print(peripheral.get_methods("right"))
